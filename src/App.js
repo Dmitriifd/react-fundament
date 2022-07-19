6e89113f7a4db962e39b878dec04ce7b34c540f9
Dmitriifd <idimka9@gmail.com>
2022-07-19 20:51:32 +0500

    сортировка, выпадающий список

--- src/App.js
+++ src/App.js
@@ -3,30 +3,50 @@ import './styles/App.css';
 import { useState } from 'react';
 import PostList from './components/PostList';
 import PostForm from './components/PostForm';
+import MySelect from './components/UI/select/MySelect';
 
 function App() {
 	const [posts, setPosts] = useState([
-		{ id: 1, title: 'Javascript 1', body: 'Description 1' },
-		{ id: 2, title: 'Javascript 2', body: 'Description 2' },
-		{ id: 3, title: 'Javascript 3', body: 'Description 3' },
+		{ id: 1, title: 'ааа', body: 'ббб' },
+		{ id: 2, title: 'ггг', body: 'ааа' },
+		{ id: 3, title: 'ввв', body: 'яяяя' },
 	]);
+
+	const [selectedSort, setSelectedSort] = useState('');
 
 	const createPost = (newPost) => {
 		setPosts([...posts, newPost]);
 	};
 
 	const removePost = (post) => {
-        setPosts(posts.filter(p => p.id !== post.id));
-    };
+		setPosts(posts.filter((p) => p.id !== post.id));
+	};
+
+	const sortPosts = (sort) => {
+        setSelectedSort(sort)
+		setPosts([...posts].sort((a,b) => a[sort].localeCompare(b[sort])))
+	};
 
 	return (
 		<div className='App'>
 			<PostForm create={createPost} />
-            {posts.length !== 0
-                ? <PostList remove={removePost} posts={posts} title={'Список постов'} />
-                : <h1 style={{textAlign: 'center'}}>Посты не были найдены</h1>
-            }
-			
+			<hr style={{ margin: '15px 0' }} />
+			<div>
+				<MySelect
+					value={selectedSort}
+					onChange={sortPosts}
+					defaultValue={'Сортировка'}
+					options={[
+						{ value: 'title', name: 'По названию' },
+						{ value: 'body', name: 'По описанию' },
+					]}
+				/>
+			</div>
+			{posts.length !== 0 ? (
+				<PostList remove={removePost} posts={posts} title={'Список постов'} />
+			) : (
+				<h1 style={{ textAlign: 'center' }}>Посты не были найдены</h1>
+			)}
 		</div>
 	);
 }
