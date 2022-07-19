f5ab4c8b68fda2a19a6da4d13f3ad3e753bfdf46
Dmitriifd <idimka9@gmail.com>
2022-07-19 15:47:55 +0500

    Первый функциональный компонент

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
-import { useState } from 'react';
+import { Counter } from './components/Counter';
 import './styles/App.css';
 
 function App() {
-	const [likes, setLikes] = useState(0);
-	const [value, setValue] = useState('Текст в инпуте');
-
-	function increment() {
-		setLikes(likes + 1);
-	}
-	function decrement() {
-		setLikes(likes - 1);
-	}
 
 	return (
 		<div className='App'>
-			<h1>{likes}</h1>
-			<h1>{value}</h1>
-            
-            {/* Двустороние связывание, управляемый инпут*/}
-            <input onChange={event => setValue(event.target.value)} type="text" value={value} />
-
-			<button onClick={increment}>Increment</button>
-			<button onClick={decrement}>Decrement</button>
+			<Counter/>
 		</div>
 	);
 }
