219c17df6abef3be54840472cbf7425a09b0085f
Dmitriifd <idimka9@gmail.com>
2022-07-19 18:18:20 +0500

    получение данных с управляемого инпута

--- src/App.js
+++ src/App.js
@@ -6,18 +6,31 @@ import MyButton from './components/UI/button/MyButton';
 import MyInput from './components/UI/input/MyInput';
 
 function App() {
-    const [posts, setPost] = useState([
-        { id: 1, title: 'Javascript 1', body: 'Description 1' },
-        { id: 2, title: 'Javascript 2', body: 'Description 2' },
-        { id: 3, title: 'Javascript 3', body: 'Description 3' },
-    ])
+	const [posts, setPost] = useState([
+		{ id: 1, title: 'Javascript 1', body: 'Description 1' },
+		{ id: 2, title: 'Javascript 2', body: 'Description 2' },
+		{ id: 3, title: 'Javascript 3', body: 'Description 3' },
+	]);
+
+	const [title, setTitle] = useState('');
+
+	const addNewPost = (e) => {
+        e.preventDefault();
+        console.log(title);
+    };
 
 	return (
 		<div className='App'>
 			<form>
-				<MyInput type='text' placeholder={'Название поста'} />
+				{/* Управляемый компонент */}
+				<MyInput
+					onChange={(e) => setTitle(e.target.value)}
+					value={title}
+					type='text'
+					placeholder={'Название поста'}
+				/>
 				<MyInput type='text' placeholder={'Описание поста'} />
-				<MyButton>Создать пост</MyButton>
+				<MyButton onClick={addNewPost}>Создать пост</MyButton>
 			</form>
 			<PostList posts={posts} title={'Список постов'} />
 		</div>
