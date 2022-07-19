c17b2a0e57cd805e79e58ce96c723b0cd8cdc619
Dmitriifd <idimka9@gmail.com>
2022-07-19 19:43:16 +0500

    Обмен данными между компонентами. От родителя к ребенку. От ребенка к родителю.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import './styles/App.css';
 
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import PostList from './components/PostList';
-import MyButton from './components/UI/button/MyButton';
-import MyInput from './components/UI/input/MyInput';
+import PostForm from './components/PostForm';
 
 function App() {
 	const [posts, setPosts] = useState([
@@ -12,38 +11,18 @@ function App() {
 		{ id: 3, title: 'Javascript 3', body: 'Description 3' },
 	]);
 
-	const [post, setPost] = useState({
-		title: '',
-		body: '',
-	});
-
-	const addNewPost = (e) => {
-		e.preventDefault();
-		
-		setPosts([...posts, {...post, id: Date.now()}]);
-		setPost({ title: '', body: '' });
+	const createPost = (newPost) => {
+		setPosts([...posts, newPost]);
 	};
+
+	const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id));
+    };
 
 	return (
 		<div className='App'>
-			<form>
-				{/* Управляемый компонент */}
-				<MyInput
-					onChange={(e) => setPost({ ...post, title: e.target.value })}
-					value={post.title}
-					type='text'
-					placeholder={'Название поста'}
-				/>
-
-				<MyInput
-					onChange={(e) => setPost({ ...post, body: e.target.value })}
-					value={post.body}
-					type='text'
-					placeholder={'Описание поста'}
-				/>
-				<MyButton onClick={addNewPost}>Создать пост</MyButton>
-			</form>
-			<PostList posts={posts} title={'Список постов'} />
+			<PostForm create={createPost} />
+			<PostList remove={removePost} posts={posts} title={'Список постов'} />
 		</div>
 	);
 }
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { PostItem } from './PostItem';
 
-const PostList = ({ posts, title }) => {
+const PostList = ({ posts, title, remove }) => {
 	return (
 		<div>
 			<h1 style={{ textAlign: 'center' }}>{title}</h1>
 			{/* key={index}  Индекс использовать не рекомендуется, поскольку при удалении и добавлении элемента индексы меняются, а ключ всегда должен быть статичным и уникальным , ключи позволяют перерисовывать не весь список а только те элементы которые изменились*/}
 			{posts.map((post, index) => (
-				<PostItem number={index + 1} post={post} key={post.id} />
+				<PostItem
+					remove={remove}
+					number={index + 1}
+					post={post}
+					key={post.id}
+                    
+				/>
 			))}
 		</div>
 	);
