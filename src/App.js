65086af0dab7b22cdbae4f9ea995d76fafeb7f4f
Dmitriifd <idimka9@gmail.com>
2022-07-21 15:53:15 +0500

    перенос роута в отдельный компонент

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import './styles/App.css';
 
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
-import About from './pages/About';
-import Posts from './pages/Posts';
 import Navbar from './components/UI/Navbar/Navbar';
-import Error from './pages/Error';
+import AppRouter from './components/AppRouter';
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Navbar />
-			<Switch>
-				<Route path='/about'>
-					<About />
-				</Route>
-				<Route path='/posts'>
-					<Posts />
-				</Route>
-				<Route path='/error'>
-					<Error />
-				</Route>
-				<Redirect to='/error' />
-			</Switch>
+			<AppRouter/>
 		</BrowserRouter>
 	);
 }
