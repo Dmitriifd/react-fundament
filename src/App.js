e1586f46e86119db8301a5e7cae671ae34cdaa45
Dmitriifd <idimka9@gmail.com>
2022-07-21 14:30:49 +0500

    Кастомный хук useFetching(). Обработка ошибок

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import MyButton from './components/UI/button/MyButton';
 import { usePosts } from './hooks/usePosts';
 import PostService from './API/PostService';
 import Loader from './components/UI/Loader/Loader';
+import { useFetching } from './hooks/useFetching';
 
 function App() {
 	const [posts, setPosts] = useState([]);
@@ -16,22 +17,18 @@ function App() {
 	const [filter, setFilter] = useState({ sort: '', query: '' });
 	const [modal, setModal] = useState(false);
 	const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-    const [isPostLoading, setIsPostLoading] = useState(false);
+	const [fetchPosts, isPostLoading, postError] = useFetching(async () => {
+		const posts = await PostService.getAll();
+		setPosts(posts);
+	});
 
 	const createPost = (newPost) => {
 		setPosts([...posts, newPost]);
 		setModal(false);
 	};
 
-	async function fetchPosts() {
-        setIsPostLoading(true);
-		const posts = await PostService.getAll();
-		setPosts(posts);
-        setIsPostLoading(false);
-	}
-
 	useEffect(() => {
-		fetchPosts();
+		fetchPosts()
 	}, []);
 
 	const removePost = (post) => {
@@ -48,13 +45,16 @@ function App() {
 			</MyModal>
 			<hr style={{ margin: '15px 0' }} />
 			<PostFilter filter={filter} setFilter={setFilter} />
-
-            {isPostLoading ? <Loader/> : <PostList
-				remove={removePost}
-				posts={sortedAndSearchedPosts}
-				title={'Список постов'}
-			/>}
-			
+            {postError && <h1>Произошла ошибка</h1>}
+			{isPostLoading ? (
+				<Loader />
+			) : (
+				<PostList
+					remove={removePost}
+					posts={sortedAndSearchedPosts}
+					title={'Список постов'}
+				/>
+			)}
 		</div>
 	);
 }
