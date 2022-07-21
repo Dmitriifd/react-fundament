7a8a5cd561c92df2c91d07ce70f6459890d9bbd3
Dmitriifd <idimka9@gmail.com>
2022-07-21 17:24:09 +0500

    Улучшаем навигацию. Приватные и публичные маршруты

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,28 +1,31 @@
 import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
-import PostIdPage from '../pages/PostIdPage';
-import Posts from '../pages/Posts';
-import About from './../pages/About';
-import Error from './../pages/Error';
+import { privateRoutes, publicRoutes } from '../router';
 
 const AppRouter = () => {
-	return (
-		<Switch>
-			<Route path='/about'>
-				<About />
-			</Route>
-			<Route exact path='/posts'>
-				<Posts />
-			</Route>
-			<Route exact path='/posts/:id'>
-				<PostIdPage />
-			</Route>
-			<Route path='/error'>
-				<Error />
-			</Route>
-			<Redirect to='/error' />
-		</Switch>
-	);
+
+		return (
+            <Switch>
+			    {privateRoutes.map((route) => (
+				<Route
+					component={route.component}
+					path={route.path}
+					exact={route.exact}
+					key={route.path}
+				/>
+			    ))}
+			    {publicRoutes.map((route) => (
+				<Route
+					component={route.component}
+					path={route.path}
+					exact={route.exact}
+					key={route.path}
+				/>
+			))}
+			    <Redirect to='/posts' />
+		    </Switch>
+        )
+	
 };
 
 export default AppRouter;
