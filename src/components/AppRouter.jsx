f47c53c6bb0a879e2e37a2d6fb9ad71af0c41643
Dmitriifd <idimka9@gmail.com>
2022-07-21 17:48:22 +0500

    useContext. Глобальные данные. Авторизация пользователя

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,31 +1,44 @@
-import React from 'react';
-import { Redirect, Route, Switch } from 'react-router-dom';
-import { privateRoutes, publicRoutes } from '../router';
+import React, {useContext} from 'react';
+import {Redirect, Route, Switch} from "react-router-dom";
+import {privateRoutes, publicRoutes} from "../router";
+import {AuthContext} from "../context";
+import Loader from "./UI/Loader/Loader";
 
 const AppRouter = () => {
+    const {isAuth, isLoading} = useContext(AuthContext);
+    console.log(isAuth)
 
-		return (
+    if (isLoading) {
+        return <Loader/>
+    }
+
+    return (
+        isAuth
+            ?
             <Switch>
-			    {privateRoutes.map((route) => (
-				<Route
-					component={route.component}
-					path={route.path}
-					exact={route.exact}
-					key={route.path}
-				/>
-			    ))}
-			    {publicRoutes.map((route) => (
-				<Route
-					component={route.component}
-					path={route.path}
-					exact={route.exact}
-					key={route.path}
-				/>
-			))}
-			    <Redirect to='/posts' />
-		    </Switch>
-        )
-	
+                {privateRoutes.map(route =>
+                    <Route
+                        component={route.component}
+                        path={route.path}
+                        exact={route.exact}
+                        key={route.path}
+                    />
+                )}
+                <Redirect to='/posts'/>
+            </Switch>
+            :
+            <Switch>
+                {publicRoutes.map(route =>
+                    <Route
+                        component={route.component}
+                        path={route.path}
+                        exact={route.exact}
+                        key={route.path}
+                    />
+                )}
+                <Redirect to='/login'/>
+            </Switch>
+    );
 };
 
 export default AppRouter;
