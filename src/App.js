41734a5eb4427786c098930d899f59f4ba523743
Dmitriifd <idimka9@gmail.com>
2022-07-21 14:04:04 +0500

    API. PostService

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import PostFilter from './components/PostFilter';
 import MyModal from './components/UI/MyModal/MyModal';
 import MyButton from './components/UI/button/MyButton';
 import { usePosts } from './hooks/usePosts';
-import axios from 'axios';
+import PostService from './API/PostService';
 
 function App() {
 	const [posts, setPosts] = useState([]);
@@ -22,10 +22,8 @@ function App() {
 	};
 
 	async function fetchPosts() {
-		const response = await axios.get(
-			'https://jsonplaceholder.typicode.com/posts'
-		);
-		setPosts(response.data);
+		const posts = await PostService.getAll();
+		setPosts(posts);
 	}
 
 	useEffect(() => {
